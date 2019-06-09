5c18257bfd89e6cc8d86454432a79878478b6cdb
anhnt2989 <anhnt2989@gmail.com>
2019-06-09 20:58:21 +0700

    nop bai

---
+++ src/main/java/ComparableCircle.java
@@ -0,0 +1,24 @@
+import javafx.scene.shape.Circle;
+import java.lang.Comparable;
+
+public class ComparableCircle extends Circle
+        implements Comparable<ComparableCircle> {
+
+    public ComparableCircle() {
+    }
+
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+
+    public ComparableCircle(double centerX, double centerY, double radius) {
+        super(centerX,centerY,radius);
+    }
+
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 1;
+    }
+}
